c31e2547a1d4da2cf5d06572b4489dddbb09dba7
Eileen Dwyer <eolson@smalltalku.com>
2016-06-02 08:56:19 -0400

    the vortex of vacuity

--- user_data.py
+++ user_data.py
@@ -1,40 +1,40 @@
 import csv
 
 
-def search_user():
-    with open("userdata.csv") as readfile:
-        data = readfile.readlines()
-    for row in data:
-        user = User()
-        for user.username in row[0]:
-            if user.password  in row[1]:
-                print("Login successful.")
-                user.create_user()
-            elif user.username in row[0] and user.password not in row[1]:
-                print("Login unsuccessful.")
-                user.username()
-            else:
-                if user.username not in row[0] and user.password not in row[1]:
-                    print("Login unsuccessful.")
-                    user.username()
-
-
 class User:
 
     def __init__(self):
-        print("Welcome.")
-        self.username = input("Enter your username to login.")
-        self.password = input("Password?")
-        self.fullname = input("Enter your full name?")
+        self.search_user()
+        self.create_user = input("Enter your full name?")
         self.age = input("Enter your age.")
 
+    def search_user(self):
+        print("Welcome.")
+        with open("userdata.csv") as readfile:
+            data = readfile.readlines()
+            self.username = input("Enter your username to login.")
+            self.password = input("Password?")
+            for row in data:
+                user = User()
+                if user.username in row[0]:
+                    if user.password in row[1]:
+                        print("Login successful.")
+                        User.create_user()
+                    elif user.username in row[0] and user.password not in row[1]:
+                        print("Login unsuccessful.")
+                        User.username()
+                    else:
+                        if user.username not in row[0] and user.password not in row[1]:
+                            print("Login unsuccessful.")
+                            User.username()
+
     def create_user(self):
-        with open("userdata.csv", "a") as writefile:
-            writefile.write("{}, {}, {}, {}".format(self.username, self.password, self.fullname, self.age))
+        with open("userdata.csv", "a") as outfile:
+            outfile.write("{}, {}, {}, {}".format(self.username, self.password, self.fullname, self.age))
         new_logout = input("(C)reate a new user or (L)ogout?")
         if new_logout.lower() == "C":
             User.create_user(self)
-            search_user()
+            User.search_user()
         else:
             print("Logging out.")
 
